f72eed15088e5849eeed9c0df6e2249e09db5966
haoyu3 <haoyu3@163.com>
2023-03-06 22:38:47 +0800

    snake 35%

--- other/snake/src/main.rs
+++ other/snake/src/main.rs
@@ -3,6 +3,7 @@ use std::process::Command;
 use std::thread::{current, sleep};
 use std::time::Duration;
 use device_query::{DeviceEvents, DeviceQuery, DeviceState, Keycode};
+use rand::Rng;
 
 const WIDTH: usize = 50;
 const HEIGHT: usize = 10;
@@ -10,9 +11,12 @@ const HEIGHT: usize = 10;
 fn main() {
 
     // 1.创建地图
-    let mut map_vec = create_map();
+    let mut map = create_map();
     // 2.创建蛇
     let mut snake = create_snake();
+    // 3.创建食物
+    let mut food = create_food();
+
     let device_state = DeviceState::new();
 
     // let _ = device_state.on_key_down(|key| {
@@ -21,17 +25,47 @@ fn main() {
     let mut current_direction = Direction::RIGHT;
     loop {
         clear_screen();
-        // 3.将蛇加入地图
-        add_snake_to_map(&snake, &mut map_vec);
-        // 打印地图与蛇
-        print_map(map_vec);
-        current_direction = input_control(&mut snake, &mut map_vec, current_direction, &device_state);
-        // move_snake(&mut snake, &mut map_vec, current_direction);
+        // 将蛇加入地图
+        add_snake_to_map(&snake, &mut map);
+        // 将食物加入地图
+        add_food_to_map(&food, &mut map);
+        // 随机生成食物
+        random_new_food(&mut food, &snake, &mut map);
+        // 打印地图、蛇、食物
+        print_map(map);
+        current_direction = input_control(&mut snake, &mut map, current_direction, &device_state);
+        // move_snake(&mut snake, &mut map, current_direction);
 
         sleep(Duration::from_millis(snake.speed));
     }
 
     //Command::new("cmd.exe").arg("/c").arg("pause").status().expect("clear error!");
+}
+
+/// 将食物加入地图
+fn add_food_to_map(food: &Food, map: &mut [[&'static str; WIDTH]; HEIGHT]) {
+    if food.eat {
+        // 食物如果被吃了，不要加到地图
+        return;
+    }
+    let position = food.position;
+    map[position[0]][position[1]] = "▣";
+}
+
+/// 随机生成食物
+fn random_new_food(food: &mut Food, snake: &Snake, map: &mut [[&'static str; WIDTH]; HEIGHT]) {
+    if !food.eat {
+        // 如果当前的食物没有被吃掉，则不生成新的食物
+        return;
+    }
+    // let head = snake.head;
+    // let body = &(snake.body);
+    let mut rng = rand::thread_rng();
+    let x = rng.gen_range(1..WIDTH - 1);
+    let y = rng.gen_range(1..HEIGHT - 1);
+    map[x][y] = "▣";
+    food.position = [x, y];
+    food.eat = false;
 }
 
 #[derive(Clone, Copy)]
@@ -40,6 +74,12 @@ enum Direction {
     DOWN,
     LEFT,
     RIGHT,
+}
+
+/// 创建食物
+fn create_food() -> Food {
+    let position = [7, 6];
+    return Food { position, eat: false };
 }
 
 /// 创建蛇
@@ -111,6 +151,12 @@ struct Snake {
     body: Vec<[usize; 2]>,
 }
 
+/// 食物
+struct Food {
+    position: [usize; 2],
+    eat: bool,
+}
+
 /// 打印地图
 fn print_map(map: [[&'static str; WIDTH]; HEIGHT]) {
     for i in 0..map.len() {
@@ -152,29 +198,7 @@ fn input_control(snake: &mut Snake,
                  map: &mut [[&'static str; WIDTH]; HEIGHT],
                  direction: Direction,
                  device_state: &DeviceState) -> Direction {
-    // ss
     let mut dir = direction;
-    // match control {
-    //     'w' => {
-    //         dir = Direction::UP;
-    //         move_snake(snake, map, Direction::UP);
-    //     }
-    //     's' => {
-    //         dir = Direction::DOWN;
-    //         move_snake(snake, map, Direction::DOWN);
-    //     }
-    //     'a' => {
-    //         dir = Direction::LEFT;
-    //         move_snake(snake, map, Direction::LEFT);
-    //     }
-    //     'd' => {
-    //         dir = Direction::RIGHT;
-    //         move_snake(snake, map, Direction::RIGHT);
-    //     }
-    //     _ => {
-    //         move_snake(snake, map, direction);
-    //     }
-    // }
     let keycodes = device_state.get_keys();
     if !keycodes.is_empty() {
         let keycode = keycodes.get(0);
