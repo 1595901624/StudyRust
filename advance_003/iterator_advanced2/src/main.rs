3b44be6bdb6aeb28f3577036623c171212f74d8b
haoyu3 <haoyu3@163.com>
2023-04-09 15:02:20 +0800

    advanced 003

--- advance_003/iterator_advanced2/src/main.rs
+++ advance_003/iterator_advanced2/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    // iter
+    // 1. iter
     let vec = vec!["hello".to_string(), "rust".to_string(), "!".to_string()];
     let mut my_iter = vec.iter();
 
@@ -7,7 +7,7 @@ fn main() {
     println!("{:?}", my_iter.next());
     println!("{:?}", my_iter.next());
 
-    // iter_mut
+    // 2. iter_mut
     let mut vec = vec!["hello".to_string(), "rust".to_string(), "!".to_string()];
     let mut my_iter_mut = vec.iter_mut();
 
@@ -21,8 +21,44 @@ fn main() {
     println!("{:?}", vec[0]);
 
     // for + iterator
-    let mut vec = vec!["hello".to_string(), "rust".to_string(), "!".to_string()];
+    let vec = vec!["hello".to_string(), "rust".to_string(), "!".to_string()];
     for item in vec.iter() {
         println!("{:?}", item);
     }
+
+    // 3. into_iterator
+    let vec = vec!["hello".to_string(), "rust".to_string(), "!".to_string()];
+    let mut iter = vec.into_iter();
+    // 集合里的第1个元素所有权转移至 first
+    let first = iter.next();
+    // 集合里的第2个元素所有权转移至 second
+    let second = iter.next();
+    // 集合里的第3个元素所有权转移至 second
+    let third = iter.next();
+
+    println!("{:?}", first);
+    println!("{:?}", second);
+    println!("{:?}", third);
+
+    // 至此 vec 已变成空壳
+    // vec 无法再使用，所有权已经转移
+    // 下面的代码将会编译失败
+    // println!("{:?}", vec);
+
+    println!("----------------------");
+    // 4. 集合引用的 into_iter
+    // 4.1 共享引用
+    let vec: Vec<String> = vec!["hello".to_string(), "rust".to_string(), "!".to_string()];
+    let mut iter = (&vec).into_iter();
+    println!("{:?}", iter.next());
+    // 下面的代码并不会编译失败
+    println!("{:?}", vec);
+    // 4.2 可修改引用
+    let mut vec: Vec<String> = vec!["hello".to_string(), "rust".to_string(), "!".to_string()];
+    let mut iter = (&mut vec).into_iter();
+    iter.next().unwrap().push_str("hello");
+    // 下面的代码并不会编译失败
+    println!("{:?}", vec);
+
+
 }
