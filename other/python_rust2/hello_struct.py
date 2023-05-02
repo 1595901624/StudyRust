91805b85dca01974480ed159da24169818903055
haoyu3 <haoyu3@163.com>
2023-05-02 09:00:27 +0800

    python2

---
+++ other/python_rust2/hello_struct.py
@@ -0,0 +1,4 @@
+import python_rust2
+
+dog = python_rust2.Dog(id = 1, name = "Shirley")
+dog.run()
--- other/python_rust2/src/lib.rs
+++ other/python_rust2/src/lib.rs
@@ -7,11 +7,35 @@ fn sort(mut vec: Vec<isize>) -> PyResult<Vec<isize>> {
     Ok(vec)
 }
 
+#[pyclass]
+struct Dog {
+    #[pyo3(get, set)]
+    pub id: isize,
+    #[pyo3(get, set)]
+    pub name: String,
+}
+
+#[pymethods]
+impl Dog {
+    #[new]
+    fn new(id: isize, name: String) -> Self {
+        Self {
+            id,
+            name,
+        }
+    }
+
+    fn run(&self) {
+        println!("{} is running!", self.name);
+    }
+}
+
 /// 一个用Rust实现的Python模块。
 ///
 /// 这个函数的名字必须与`Cargo.toml`中的`lib.name`匹配
 #[pymodule]
 fn python_rust2(_py: Python, module: &PyModule) -> PyResult<()> {
     module.add_function(wrap_pyfunction!(sort, module)?)?;
+    module.add_class::<Dog>()?;
     Ok(())
 }
