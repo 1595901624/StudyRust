5024eb35cff655633fabb6a1a819eb6cb9995f30
haoyu3 <haoyu3@163.com>
2023-07-22 00:50:57 +0800

    code10

--- action/code10/src/main.rs
+++ action/code10/src/main.rs
@@ -2,10 +2,18 @@ mod log;
 mod log_macro;
 mod test;
 
+
 fn main() {
     println!("Hello, world!");
 
     // std::string::String::from("Hello, world!");
     info!(format!("aad {}", 5));
+
+    // macro_rules! my_println {
+    //     ($arg:expr) => {
+    //         println!("The argument is: {}\n", $arg);
+    //     };
+    // }
+    // my_println!("rust");
 }
 
--- action/code10/src/test.rs
+++ action/code10/src/test.rs
@@ -1,21 +1,29 @@
 #[test]
 fn test_my_print() {
-
-
-
-
-
     macro_rules! my_println {
         ($arg:expr) => {
             println!("The argument is: {}\n", $arg);
         };
     }
-
-
-
-
-
     my_println!("rust");
+}
+
+/// 重复
+#[test]
+fn test_repeat() {
+    macro_rules! repeat {
+        ($($value:expr),*) => {
+        {
+                let mut temp_vec = Vec::new();
+                $(
+                    temp_vec.push($value);
+                )* // 这里的 * 表示重复 0 到多次
+                temp_vec
+            }
+        };
+    }
+    let vec = repeat!(1,2,3,4,5);
+    println!("vec = {:?}", vec);
 }
 
 #[test]
@@ -29,4 +37,5 @@ fn test_macro() {
     let sum = add!(5.0, 10.0);
     println!("sum = {}", sum);
 }
+
 
