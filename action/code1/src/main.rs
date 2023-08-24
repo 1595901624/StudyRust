8682f81fddbb80a7f15e80baf3a8f0b13346c058
haoyu3 <haoyu3@163.com>
2023-08-24 21:06:31 +0800

    update 01

--- action/code1/src/main.rs
+++ action/code1/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let value = [5, 12, 23, 45, 65, 66, 87, 98];
+    let value = vec![5, 12, 23, 45, 65, 66, 87, 98];
     let result = binary_search(&value, 12);
     match result {
         Some(index) => {
@@ -23,7 +23,7 @@ fn binary_search(value: &[i32], element: i32) -> Option<usize> {
         let mid = start + (end - start) / 2;
         // 如果待查找元素与中间元素相等，则查找成功，返回 mid
         if value[mid] == element {
-            // mid 默认是 usize 类型，需要使用 as 转换为 i32 类型
+            // 返回 mid
             return Some(mid);
         } else if value[mid] > element {
             // 如果待查找元素比中间元素小，则更新 end = mid - 1，
@@ -33,7 +33,7 @@ fn binary_search(value: &[i32], element: i32) -> Option<usize> {
             start = mid + 1;
         }
     }
-    // 如果 start > end，则表明数组中不存在待查找元素，则查找失败，返回默认值 -1
+    // 如果 start > end，则表明数组中不存在待查找元素，则查找失败，返回默认值 None
     return None;
 }
 
@@ -43,11 +43,19 @@ fn test() {
     let array = vec![1, 4, 7, 9, 8, 6, 4, 2];
     let target = 2;
     let result = find_in_mountain_array(target, array);
-    assert_eq!(7, result);
+    match result {
+        None => {
+            println!("未找到元素");
+        }
+        Some(index) => {
+            println!("找到元素，索引值为：{}", index);
+            assert_eq!(7, index);
+        }
+    }
 }
 
 /// 山脉数组中查找目标值
-pub fn find_in_mountain_array(target: i32, array: Vec<i32>) -> i32 {
+pub fn find_in_mountain_array(target: i32, array: Vec<i32>) -> Option<usize> {
     // 首先通过二分法找山顶
     let mut start = 0;
     let mut end = array.len() - 1;
@@ -63,14 +71,14 @@ pub fn find_in_mountain_array(target: i32, array: Vec<i32>) -> i32 {
     // 封顶值是 start
     let peak = start;
     let result = bs(target, &array, 0, peak, false);
-    if result != -1 {
+    if result != None {
         return result;
     }
     return bs(target, &array, peak + 1, array.len() - 1, true);
 }
 
 /// 二分查找
-fn bs(target: i32, array: &Vec<i32>, mut start: usize, mut end: usize, rev: bool) -> i32 {
+fn bs(target: i32, array: &Vec<i32>, mut start: usize, mut end: usize, rev: bool) -> Option<usize> {
     // 获取新的 target 值
     // 为了实现代码复用，我们仅写一个二分查找的方法
     // 对于右侧的值，如果我们将当前的值 * -1，则将变成升序排列
@@ -90,8 +98,8 @@ fn bs(target: i32, array: &Vec<i32>, mut start: usize, mut end: usize, rev: bool
         let current_element = array[mid] * if rev { -1 } else { 1 };
         // 如果待查找元素与中间元素相等，则查找成功，返回 mid
         if current_element == new_target {
-            // mid 默认是 usize 类型，需要使用 as 转换为 i32 类型
-            return mid as i32;
+            // 返回 mid
+            return Some(mid);
         } else if current_element > new_target {
             // 如果待查找元素比中间元素小，则更新 end = mid - 1，
             end = mid - 1;
@@ -100,6 +108,6 @@ fn bs(target: i32, array: &Vec<i32>, mut start: usize, mut end: usize, rev: bool
             start = mid + 1;
         }
     }
-    // 如果 start > end，则表明数组中不存在待查找元素，则查找失败，返回默认值 -1
-    return -1;
+    // 如果 start > end，则表明数组中不存在待查找元素，则查找失败，返回默认值 None
+    return None;
 }
