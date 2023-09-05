7a7243b49628437770aa9aa059192d9f3baf2b42
haoyu <haoyu3@163.com>
2023-09-05 19:03:46 +0800

    更新代码

--- slintui/005/element-locate/src/main.rs
+++ slintui/005/element-locate/src/main.rs
@@ -3,11 +3,64 @@ fn main() {
 }
 slint::slint! {
     // 自动定位
-    export component App inherits Window {
+    import { Button } from "std-widgets.slint";
+export component App inherits Window {
         width: 300px;
         height: 300px;
         background: red;
-      
+
+    }
+
+    // 垂直布局
+    component VerticalLayoutTest {
+        width: 300px;
+        height: 300px;
+
+        VerticalLayout {
+            width: 300px;
+            height: 300px;
+
+            Rectangle {
+                height: 100px;
+                background: blue;
+            }
+
+            Rectangle {
+                height: 100px;
+                background: red;
+            }
+
+            Rectangle {
+                height: 100px;
+                background: green;
+            }
+        }
+    }
+
+    // 水平布局
+    component HorizontalLayoutTest {
+        width: 300px;
+        height: 300px;
+
+        HorizontalLayout {
+            width: 300px;
+            height: 300px;
+
+            Rectangle {
+                width: 100px;
+                background: blue;
+            }
+
+            Rectangle {
+                width: 100px;
+                background: red;
+            }
+
+            Rectangle {
+                width: 100px;
+                background: green;
+            }
+        }
     }
 
 }
