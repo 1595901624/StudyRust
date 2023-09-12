4ac8c57cfe25186e00ae7cbbbfd3c3f5306c858d
haoyu <haoyu3@163.com>
2023-09-12 18:45:37 +0800

    add slint06

--- slintui/006/vh2/src/main.rs
+++ slintui/006/vh2/src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
     println!("Hello, world!");
+    App::new().unwrap().run().unwrap();
+}
+
+slint::slint! {
+    export component App inherits Window {
+        width: 400px;
+        height: 400px;
+    }
+
+    component HorizontalStretchComponment inherits Rectangle {
+        width: 500px;
+        height: 500px;
+
+        HorizontalLayout {
+
+            Rectangle {
+                horizontal-stretch: 0;
+                min-width: 100px;
+                background: red;
+            }
+            Rectangle {
+                horizontal-stretch: 1;
+                min-width: 200px;
+                background: green;
+            }
+            Rectangle {
+                horizontal-stretch: 2;
+                min-width: 100px;
+                background: blue;
+            }
+        }
+    }
 }
