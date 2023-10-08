559b60979a20e73e2ec8cee907f1686a915618e9
haoyu3 <haoyu3@163.com>
2023-10-08 20:49:50 +0800

    a15

--- advance_015/test_demo/src/main.rs
+++ advance_015/test_demo/src/main.rs
@@ -1,4 +1,17 @@
 fn main() {
     println!("Hello, world!");
+}
 
+fn fibonacci(n: u32) -> u32 {
+    if n <= 1 {
+        n
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    }
+}
+
+#[test]
+fn test_fibonacci() {
+    let x = fibonacci(10);
+    println!("fib(10) = {}", x);
 }
