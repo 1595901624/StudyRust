cb24dd78990db2835f5bd5dff0ca822a45bd9b5d
haoyu <haoyu3@163.com>
2023-10-25 17:59:56 +0800

    a17

--- advance_017/regex_demo/src/main.rs
+++ advance_017/regex_demo/src/main.rs
@@ -2,8 +2,21 @@ use regex::Regex;
 
 fn main() {
 
-    // 匹配手机号
+}
+
+
+/// 匹配手机号
+#[test]
+fn test_phone_regex() {
     let regex = Regex::new(r#"^1[0-9]{10}$"#).unwrap();
     let match_result = regex.is_match("16688889999");
-    println!("{}", match_result);
+    assert!(match_result);
+}
+
+/// 匹配邮箱
+#[test]
+fn test_email_regex() {
+    let regex = Regex::new(r#"^[a-zA-Z0-9]{1,}@[a-zA-Z0-9]{1,}\.[a-zA-Z0-9]{1,}$"#).unwrap();
+    let match_result = regex.is_match("rust@example.com");
+    assert!(match_result);
 }
