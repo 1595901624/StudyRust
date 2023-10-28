b7b4417c49d2b10b648a663cd3c20c6603cae482
haoyu3 <haoyu3@163.com>
2023-10-28 11:28:49 +0800

    a17

--- advance_017/regex_demo/src/main.rs
+++ advance_017/regex_demo/src/main.rs
@@ -1,8 +1,6 @@
 use regex::Regex;
 
-fn main() {
-
-}
+fn main() {}
 
 
 /// 匹配手机号
@@ -19,4 +17,22 @@ fn test_email_regex() {
     let regex = Regex::new(r#"^[a-zA-Z0-9]{1,}@[a-zA-Z0-9]{1,}\.[a-zA-Z0-9]{1,}$"#).unwrap();
     let match_result = regex.is_match("rust@example.com");
     assert!(match_result);
+}
+
+
+#[test]
+fn test_regex_find() {
+    let regex = Regex::new(r#"1[0-9]{3}"#).unwrap();
+    let haystack = "155,1888,1999,177,12,1666";
+    let match_result = regex.find(haystack);
+    println!("match_result: {:?}", match_result);
+}
+
+#[test]
+fn test_regex_find_iter() {
+    let regex = Regex::new(r#"1[0-9]{3}"#).unwrap();
+    let haystack = "155,1888,1999,177,12,1666";
+    for mat in regex.find_iter(haystack) {
+        println!("match_result: {}", mat.as_str());
+    }
 }
