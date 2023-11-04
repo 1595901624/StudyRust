e120ceef51eee92ef633fd3edbbb2f209850691c
haoyu3 <haoyu3@163.com>
2023-11-04 19:55:01 +0800

    a18

--- advance_018/file_demo/src/main.rs
+++ advance_018/file_demo/src/main.rs
@@ -1,4 +1,13 @@
+use std::ffi::{OsStr, OsString};
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let s1 = OsStr::new("hello");
+    let s2 = s1.to_os_string();
+
+    println!("{:?}", s1);
+    println!("{:?}", s2);
+
+
 }
 
