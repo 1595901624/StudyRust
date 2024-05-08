b32837248d6e78f745165df325d017703278c8a2
haoyu3 <haoyu3@163.com>
2024-05-08 19:47:31 +0800

    28

--- advance_028/unsafe_demo/src/main.rs
+++ advance_028/unsafe_demo/src/main.rs
@@ -1,9 +1,20 @@
+use std::str::{from_utf8, from_utf8_unchecked};
+
 fn main() {
-    println!("Hello, world!");
+    let rust = vec![72, 101, 108, 108, 111, 44, 32, 82, 117, 115, 116, 33];
+    println!("安全版本 rust: {}", from_utf8(&rust).unwrap());
 
     unsafe {
-        let x = "rust".to_string();
-        x.get_unchecked()
+        println!("不安全版本 rust: {}", from_utf8_unchecked(&rust));
+    }
+
+    // 255 是一个错误的编码
+    let err_rust = vec![72, 101, 108, 108, 111, 44, 32, 82, 117, 115, 116, 33, 255];
+
+    println!("安全版本 err_rust: {:#?}", from_utf8(&err_rust));
+
+    unsafe {
+        println!("不安全版本 err_rust: {}", from_utf8_unchecked(&err_rust));
     }
 }
 
