77ca961e39339f110d56e5f116715c5fa7bbbbc4
haoyu3 <haoyu3@163.com>
2024-06-01 14:00:00 +0800

    30

--- advance_030/ffi/src/main.rs
+++ advance_030/ffi/src/main.rs
@@ -1,3 +1,30 @@
+use std::ffi::c_void;
+
 fn main() {
     println!("Hello, world!");
+    let a: std::os::raw::c_short = 3;
+    let b: std::ffi::c_short = 4;
+    println!("{}", a);
+    println!("{}", b);
+
+    unsafe {
+        let ptr = malloc(10 * std::mem::size_of::<i32>());
+        println!("{:?}", ptr);
+
+        let b = -5;
+        let c = abs(b);
+        println!("{}", c);
+
+        let d = getch();
+        println!("{}", d);
+    }
+}
+
+// 声明 C 标准库函数 `malloc`
+extern "C" {
+    fn malloc(size: usize) -> *mut c_void;
+
+    fn abs(i: i32) -> i32;
+
+    fn getch() -> i32;
 }
